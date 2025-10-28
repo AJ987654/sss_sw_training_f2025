557054075b57c40755a4b7fed84ee1a06fb7cb2a
Achintya Jaimini <ajaimini@ucdavis.edu>
2025-10-28 16:07:07 -0700

    commit

--- Core/Src/training/gpio_training.c
+++ Core/Src/training/gpio_training.c
@@ -3,25 +3,34 @@
  *
  * FOR SSS SOFTWARE FALL 2025 TRAINING
  *
- *
  *  Created on: Sep 21, 2025
  *      Author: nithinsenthil
  */
 
 #include "gpio_training.h"
 
-void gpio_high(GPIO_TypeDef *port, int pin) {}
-
-void gpio_low(GPIO_TypeDef *port, int pin) {}
-
-void gpio_set(GPIO_TypeDef *port, int pin, GPIO_VALUE value) {}
-
-void gpio_configureMode(GPIO_TypeDef *port, int pin, GPIO_CNF cnf, GPIO_MODE mode) {}
-
-/*
- * Pls don't modify cause I will be sad D:
- */
-void rcc_enable(RCC_SELECT reg) {
-	RCC->APB2ENR |= reg;
+void gpio_toggle(GPIO_TypeDef *port, int pin) {
+	// Sets it to opposite (on -> off) and (off -> on)
+	*(volatile uint32_t *)(0x4001100C) ^= (1 << 13);
 }
 
+void gpio_set(GPIO_TypeDef *port, int pin, GPIO_VALUE value) {
+	// Check the state of the current GPIO
+    if (value == GPIO_HIGH) {
+    	*(volatile uint32_t *)(0x4001100C) |= (1 << 13);
+    } else {
+    	*(volatile uint32_t *)(0x4001100C) &= ~(1 << 13);
+    }
+}
+
+void gpio_configureMode(GPIO_TypeDef *port, int pin, GPIO_CNF cnf, GPIO_MODE mode) {
+	int *reg = (pin < 8) ? &port->CRL : &port->CRH;
+	*(reg) = (*(reg) & ~(0xF << ((pin % 8) * 4))) | (((cnf << 2) | mode) << ((pin % 8) * 4));
+}
+
+/*
+ * Please don't modify — this one just enables GPIO clocks :)
+ */
+void rcc_enable(RCC_SELECT reg) {
+    RCC->APB2ENR |= reg;
+}
--- Core/Src/training/main_training.c
+++ Core/Src/training/main_training.c
@@ -5,28 +5,25 @@
  *      Author: dilgill
  */
 
-#include "FreeRTOS.h"
-#include "task.h"
-#include "semphr.h"
-
-#include "main.h"
-
+#include "stm32f1xx.h"
 #include "gpio_training.h"
+#include "main.h"
+#include <stdbool.h>
 
 #define LED_PORT GPIOC
 #define LED_PIN  13
 
-int main(void)
-{
-	setup_training();
-
-	int count = 0;
-
-	/* Infinite loop */
-	while (1)
-	{
-	  HAL_Delay(1000);
-	  printMsg("Hello #%d\r\n", count++);
-	}
+int main(void) {
+    setup_training();
+    rcc_enable(GPIO_C_ENABLE);
+    rcc_enable(GPIO_A_ENABLE);
+    gpio_configureMode(LED_PORT, LED_PIN, GPIO_OUTPUT_PUSH_PULL, GPIO_LOW_SPEED);
+    for (int i = 0; i < 4; i++) {
+        gpio_toggle(LED_PORT, LED_PIN);  // LED OFF
+        HAL_Delay(1000);
+    }
+    while (true) {
+        gpio_toggle(LED_PORT, LED_PIN);         // Toggle LED
+        HAL_Delay(450);
+    }
 }
-
